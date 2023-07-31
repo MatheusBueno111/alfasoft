d8e0a8934d8935c8140ecebb9bfbb1a43eca92a4
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2023-07-30 22:09:16 -0300

    add nav button

--- src/pages/NewContact/index.tsx
+++ src/pages/NewContact/index.tsx
@@ -3,11 +3,20 @@ import Header from '../../components/Header'
 
 import * as S from './styles'
 import CreateNewContact from '../../components/CreateNewContact'
+import { Link } from 'react-router-dom'
+import { House } from 'phosphor-react'
 
 const NewContact: React.FC = () => {
   return (
     <S.Container>
       <Header />
+      <S.Wrapper>
+        <Link to="/">
+          <House weight="bold" size={16} />
+          <span>Home</span>
+        </Link>
+      </S.Wrapper>
+
       <CreateNewContact />
     </S.Container>
   )
--- src/pages/NewContact/styles.ts
+++ src/pages/NewContact/styles.ts
@@ -8,3 +8,28 @@ export const Container = styled.div`
   margin: 0 auto;
   justify-content: space-between;
 `
+
+export const Wrapper = styled.div`
+  display: flex;
+
+  width: fit-content;
+  align-self: end;
+  gap: 1rem;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: ${({ theme }) => theme.colors.white};
+
+  margin-top: 1rem;
+  border-radius: 0.4rem;
+  padding: 1.6rem;
+  font-size: 1.6rem;
+  font-weight: 700;
+
+  a {
+    all: unset;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    cursor: pointer;
+  }
+`
